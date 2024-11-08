375bcb2c678667df1905f150d53091fb0cf596e5
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-08 18:31:48 +0530

    add handleSubmit function to handle form submit

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,11 +1,16 @@
 function SearchForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const searchedValue = e.target.elements.search.value;
+    console.log(searchedValue);
+  }
   return (
     <section>
       <h1 className="title">unsplash images</h1>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="text"
-          className="form-input search"
+          className="form-input search-input"
           name="search"
           placeholder="cat"
         />
