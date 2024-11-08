a4f710cd0debb99d1ad264c3818f783d8a09a183
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-08 18:22:25 +0530

    add button in the form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,7 +3,13 @@ function SearchForm() {
     <section>
       <h1 className="title">unsplash images</h1>
       <form className="search-form">
-        <input type="text" className="form-input search" />
+        <input
+          type="text"
+          className="form-input search"
+          name="search"
+          placeholder="cat"
+        />
+        <button className="btn">search</button>
       </form>
     </section>
   );
