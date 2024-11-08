1851e32c1557791339574ce958c295f3f450b2cf
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-08 18:21:14 +0530

    create a form input componet for searching images

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,12 @@
 function SearchForm() {
-  return <div>Form</div>;
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form">
+        <input type="text" className="form-input search" />
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
